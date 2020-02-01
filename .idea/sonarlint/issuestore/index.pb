b31be12dc88782d52eca8387c17b8003f90ed773
Dhaval Dedhia <dhavl32@gmail.com>
2020-01-31 21:37:22 -0500

    Added the services for course CRUD operations

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+;
+src/App.css,a\a\aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
+<
+src/index.js,f\9\f900edc44cb929c2267a16100a32851f6d5b477d
+=
+src/index.css,c\5\c5d38a62d1faae90a2acaa34b79a7ef365c917e4
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+M
+src/services/CourseService.js,1\3\134b94d760aa1da497d64432dfd6212a6239ede0
---
+++ src/services/CourseService.js
@@ -0,0 +1,43 @@
+function CourseServices() {
+    API_URL = "http://wbdv-generic-server.herokuapp.com/api/001835414/courses"
+
+    export const createCourse = async (course) => {
+        const response = await fetch(API_URL,{
+            method : 'POST',
+            body : JSON.stringify(course),
+            headers: {
+                'content-type' : 'application.json'
+            }
+        })
+        return await response.json()
+    }
+}
+
+    export const findCourseById = async (courseId) => {
+        const response = await fetch(API_URL+"/"+courseId)
+        return await response.json()
+    }
+
+    export const updateCourse = async (course, courseId) => {
+        const reponse = await fetch(API_URL+"/"+courseId, {
+            method : 'PUT',
+            body : JSON.stringify(course),
+            headers : {
+                'content-type' : 'application/json'
+            }
+        })
+        return await response.json()
+    }
+
+    export const deleteCourse = async (courseId) => {
+        const response = await fetch(API_URL+"/"+courseId,{
+            method : 'DELETE'
+        })
+        return await response.json()
+    }
+
+    export const findAllCourses = async () => {
+        const response = await fetch(API_URL)
+        return await response.json()
+
+    }
