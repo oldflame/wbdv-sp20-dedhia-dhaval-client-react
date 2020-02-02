3338972f005b585572887b97e7be2d0278cd8311
Dhaval Dedhia <dhavl32@gmail.com>
2020-02-01 23:49:30 -0500

    Added rest of structured files

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,17 +1,15 @@
-function CourseServices() {
-    API_URL = "http://wbdv-generic-server.herokuapp.com/api/001835414/courses"
+    const API_URL = "http://wbdv-generic-server.herokuapp.com/api/j4inam/courses"
 
     export const createCourse = async (course) => {
         const response = await fetch(API_URL,{
             method : 'POST',
             body : JSON.stringify(course),
             headers: {
-                'content-type' : 'application.json'
+                'content-type' : 'application/json'
             }
         })
         return await response.json()
-    }
-}
+    }   
 
     export const findCourseById = async (courseId) => {
         const response = await fetch(API_URL+"/"+courseId)
@@ -19,7 +17,7 @@ function CourseServices() {
     }
 
     export const updateCourse = async (course, courseId) => {
-        const reponse = await fetch(API_URL+"/"+courseId, {
+        const response = await fetch(API_URL+"/"+courseId, {
             method : 'PUT',
             body : JSON.stringify(course),
             headers : {
@@ -31,7 +29,10 @@ function CourseServices() {
 
     export const deleteCourse = async (courseId) => {
         const response = await fetch(API_URL+"/"+courseId,{
-            method : 'DELETE'
+            method : 'DELETE',
+            headers : {
+                'content-type' : 'application/json'
+            }
         })
         return await response.json()
     }
