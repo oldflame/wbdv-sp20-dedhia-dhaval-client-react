4cd61b028eed96dd3918593e08a526199f3ff703
Dhaval Dedhia <dhavl32@gmail.com>
2020-02-01 23:47:18 -0500

    Added Navbar component and styling

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import './NavBar.css'
+
+class NavBar extends Component {
+  state = { 
+    courseTitle : ''
+   }
+
+
+   handleInputChange = (event) => {
+     this.setState({courseTitle : event.target.value})
+   }
+
+
+
+
+  render() { 
+    return (
+      <div className="">
+      <nav className="navbar navbar-light justify-content-between nav-bg">
+        <a className="navbar-brand text-white">{this.props.title}</a>
+    
+          <input
+            className=" course-title-input form-control mr-sm-2"
+            type="search"
+            value={this.state.courseTitle}
+            placeholder="Search"
+            aria-label="Search"
+            onChange={this.handleInputChange}
+          />
+        <i className="plus-icon fa fa-2x fa-plus" onClick={()=>this.props.onAddCourse(this.state.courseTitle)}></i>
+      </nav>
+    </div>
+      );
+  }
+}
+ 
+export default NavBar;
