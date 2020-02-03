2d67fc4fa20d6f2e2e2fd2a2ba831094138e94fd
Dhaval Dedhia <dhavl32@gmail.com>
2020-02-02 19:49:24 -0500

    Added Course Editor navigation bar on the course editor page

---
+++ src/components/NavBarCourseEditor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const NavBarCourseEditor = () => {
+    return (  
+        <div className="">
+      <nav className="navbar navbar-light justify-content-between nav-bg">
+        <a className="navbar-brand text-white"></a>
+      </nav>
+    </div>
+    );
+}
+ 
+export default NavBarCourseEditor;
--- src/containers/CourseEditor.jsx
+++ src/containers/CourseEditor.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
+import ModuleList from "../components/ModuleList";
+import NavBarCourseEditor from "../components/NavBarCourseEditor";
 
 class CourseEditor extends Component {
   state = {};
   render() {
     return (
       <div>
+        <NavBarCourseEditor />
         <nav className="navbar navbar-light navbar-color">
           <form className="form-inline">
             <div>
               <a className="navbar-brand navbar-text-color" href="#">
-                <i class="fa fa-times mr-3 wbdv-course-title wbdv-course-editor wbdv-close" onClick={this.props.toggleEditor}></i>
-                CS 5610 Web Development Graduate
+                <i
+                  class="fa fa-times mr-3 wbdv-course-title wbdv-course-editor wbdv-close"
+                  onClick={this.props.toggleEditor}
+                ></i>
               </a>
             </div>
           </form>
         </nav>
+        <ModuleList moduleTitle="jQuery" />
+        <ModuleList moduleTitle="React"/>
+        <ModuleList moduleTitle="Redux"/>
+        <ModuleList moduleTitle="Native"/>
+        <ModuleList moduleTitle="Angular"/>
+        <ModuleList moduleTitle="Node"/>
+        <ModuleList moduleTitle="Mongo"/>
       </div>
     );
   }
