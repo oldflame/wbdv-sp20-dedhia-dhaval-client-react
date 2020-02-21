de71ef54fb77bcdc49c067eb5ce2f019cf7c4b04
Dhaval Dedhia <dhavl32@gmail.com>
2020-02-20 23:01:00 -0500

    Added the Widget Service

---
+++ src/services/WidgetService.js
@@ -0,0 +1,50 @@
+const API_URL = "https://localhost:8080/api/"
+
+
+export const createWidget = async (widgetId, widget) => {
+    const response = await fetch(`${API_URL}topics/${topicId}/widgets`,{
+        method : 'POST',
+        body  :JSON.stringify(widget),
+        headers: {
+            'content-type' : 'application/json'
+        }
+    })
+    return await response.json()
+}
+
+export const findWidgetsForTopic = async (topicId) => {
+    const response = await fetch(`${API_URL}topic/${topicId}/widgets`)
+        return await response.json()
+
+}
+
+export const updateWidget = async (widgetId,widget) => {
+    const response = await fetch(`${API_URL}widgets/${widgetId}/`,{
+        method : 'PUT',
+        body : JSON.stringify(widget),
+        headers : {
+            'content-type' : 'application/json'
+        }
+    })
+    return await response.json()
+}
+
+
+
+export const deleteWidget = async (widgetId) => {
+    const response = await fetch(`${API_URL}widgets/${widgetId}`,{
+        method : 'DELETE',
+        headers : {
+            'content-type' : 'application/json'
+        }
+    })
+    return await response.json()
+}
+
+
+export default {
+    createWidget,
+    updateWidget,
+    deleteWidget,
+    findWidgetsForTopic
+}
