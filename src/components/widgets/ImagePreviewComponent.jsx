1c89efb73425c4e309b4abd5782429bce9e64f9b
Dhaval Dedhia <dhavl32@gmail.com>
2020-03-09 16:26:15 -0400

    Added files for image and list widget preview components

---
+++ src/components/widgets/ImagePreviewComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const ImagePreviewComponent = () => {
+    return (  
+        <div>
+            
+        </div>
+    );
+}
+ 
+export default ImagePreviewComponent;
---
+++ src/components/widgets/ListPreviewComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const ListPreviewComponent = () => {
+    return (  
+        <div>
+            
+        </div>
+    );
+}
+ 
+export default ListPreviewComponent;
